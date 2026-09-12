c86c60970a034959d2804c4d0daf46f37bec392b
sshadrin2 <s.shadrin2@g.nsu.ru>
2026-09-12 21:44:58 +0700

    fix reviewdog

--- Task_1_1_1/src/test/java/ru/nsu/sshadrin2/heapsort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/sshadrin2/heapsort/SortTest.java
@@ -1,10 +1,9 @@
 package ru.nsu.sshadrin2.heapsort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
 import java.util.Random;
-
 import org.junit.jupiter.api.Test;
 
 class SortTest {
