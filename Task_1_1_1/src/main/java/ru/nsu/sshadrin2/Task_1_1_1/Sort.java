b69752b49aaf2b9403a8cca3fdeb05cf52bf0715
sshadrin2 <s.shadrin2@g.nsu.ru>
2026-09-12 19:59:29 +0700

    implement binary heap

--- Task_1_1_1/src/main/java/ru/nsu/sshadrin2/Task_1_1_1/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/sshadrin2/Task_1_1_1/Sort.java
@@ -1,15 +1,71 @@
 package ru.nsu.sshadrin2.Task_1_1_1;
 
-import java.util.PriorityQueue;
+class HeapInt {
+    private int[] heap;
+    private int size;
+    private int cap;
+    public HeapInt(int cap) {
+        this.cap = cap;
+        this.size = 0;
+        this.heap = new int[cap];
+        for (int i = 0; i < cap; i++) {
+            heap[i] = Integer.MAX_VALUE;
+        }
+    }
+    private void siftUp(int index) {
+        while (index > 0) {
+            int v = (index - 1) / 2;
+            if (heap[v] <= heap[index]) {
+                return;
+            }
+            int temp = heap[v];
+            heap[v] = heap[index];
+            heap[index] = temp;
+            index = v;
+        }
+    }
+    private void siftDown(int index) {
+        while (index < size) {
+            int l = 2 * index + 1;
+            int r = 2 * index + 2;
+            int min_index = index;
+            if (l < size && heap[l] < heap[min_index]) {
+                min_index = l;
+            }
+            if (r < size && heap[r] < heap[min_index]) {
+                min_index = r;
+            }
+            if (index == min_index) return;
+            int temp = heap[min_index];
+            heap[min_index] = heap[index];
+            heap[index] = temp;
+            index = min_index;
+        }
+    }
+    public void insert(int value) {
+        heap[size++] = value;
+        siftUp(size - 1);
+    }
+    public int extract() {
+        int res = heap[0];
+        size--;
+        heap[0] = heap[size];
+        heap[size] = Integer.MAX_VALUE;
+        siftDown(0);
+        return res;
+    }
 
+}
 public class Sort {
+
+
     public static int[] sort(int[] array) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        HeapInt heap = new HeapInt(array.length);
         for (int i = 0; i < array.length; i++) {
-            heap.add(array[i]);
+            heap.insert(array[i]);
         }
         for (int i = 0; i < array.length; i++) {
-            array[i] = heap.remove();
+            array[i] = heap.extract();
         }
         return array;
     }
--- Task_1_1_1/src/test/java/ru/nsu/sshadrin2/Task_1_1_1/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/sshadrin2/Task_1_1_1/SortTest.java
@@ -2,18 +2,57 @@ package ru.nsu.sshadrin2.Task_1_1_1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortTest {
 
+//    @Test
+//    void sort() {
+//        int[] array = new int[]{1, 3, 2};
+//        var result = Sort.sort(array);
+//        assertArrayEquals(new int[]{1, 2, 3}, result);
+//
+//        array = new int[]{5, 4, 3, 2, 1};
+//        result = Sort.sort(array);
+//        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, result);
+//    }
     @Test
-    void sort() {
-        int[] array = new int[]{1, 3, 2};
+    void emptyArray() {
+        int[] array = {};
         var result = Sort.sort(array);
-        assertArrayEquals(new int[]{1, 2, 3}, result);
+        assertArrayEquals(new int[]{}, result);
+    }
+    @Test
+    void singleElementArray() {
+        int[] array = {10};
+        var result = Sort.sort(array);
+        assertArrayEquals(new int[]{10}, result);
+    }
 
-        array = new int[]{5, 4, 3, 2, 1};
-        result = Sort.sort(array);
+    @Test
+    void descendingArray() {
+        int[] array = {5, 4, 3, 2, 1};
+        var result = Sort.sort(array);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, result);
+    }
+    @Test
+    void minMaxValuesArray() {
+        int[] array = {Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
+        var result = Sort.sort(array);
+        assertArrayEquals(new int[]{Integer.MIN_VALUE, 0, Integer.MAX_VALUE}, result);
+    }
+    @Test
+    void randomArray() {
+        Random rnd = new Random(44);
+        int[] array = new int[10000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd.nextInt();
+        }
+        var result = Sort.sort(array);
+        Arrays.sort(array);
+        assertArrayEquals(array, result);
     }
 }
